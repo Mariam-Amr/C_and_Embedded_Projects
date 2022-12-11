2632c7243a434aa13d2c52d0acf2c949bcaee293
Mariam (Mëmø) <33464259+Mariam-Amr@users.noreply.github.com>
2022-12-11 11:29:01 +0200

    Update Client.h

--- ITI_Bank_Sysyem/Client.h
+++ ITI_Bank_Sysyem/Client.h
@@ -6,5 +6,6 @@ void Client_Make_Transaction();
 void Client_Get_Cash();
 void Client_Deposit_in_Account();
 void Client_Change_Account_Password();
-
+void close(void);
+void fordelay(long long int j);
 #endif // CLIENT_H_INCLUDED

9faf2838b016ba5f192946150db86f172e8c704e
Mariam (Mëmø) <33464259+Mariam-Amr@users.noreply.github.com>
2022-12-11 13:46:18 +0200

    Update Admin.h

--- ITI_Bank_Sysyem/Admin.h
+++ ITI_Bank_Sysyem/Admin.h
@@ -6,7 +6,7 @@
 #include<windows.h>
 
   typedef struct date{                  // struct to define the date important while enquiring about
-    int month;                //deposits ,withdraw or transaction dates
+    int month;                         //deposits ,withdraw or transaction dates
     int day;
     int year;
 };
@@ -17,16 +17,15 @@
     char   name[60];
     char   address[100];
     char   national_id[14];
-    char   Garduian_national_id[14];
-    char   account_id[6];
+    int    account_id[6];
     int    age;
+    char   password[5];
     float  amount_to_deposite;
     char   gardian_national_id[14];       // if age is less than 21
-    char   account_staus[10];            //active is default
     struct date deposit;
     struct date withdraw;
-//    Status account_staus ;
-    }add,upd,check,rem,transaction;     //objects from struct
+    Status account_staus ;   //active is default
+    }add,upd,check,rem,transaction,deposite;     //objects from struct
 
 //----------Admin resbonsibilities-------------
 
@@ -38,7 +37,8 @@ void Change_Account_Status();
 void Get_Cash();
 void Deposit_in_Account();
 void Randum_Password_Generation();
-void fordelay(int j);        //delay function  wait till the next menu will appear
+void view_list();
+void fordelay(long long int j);        //delay function  wait till the next menu will appear
 void close(void);
 
 #endif // ADMIN_H_INCLUDED
